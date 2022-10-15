ac2ada16563aca1a9c5a531bdb3f2d53ea97ad79
Marcos Huck <marcos@huck.com.ar>
2022-10-15 19:06:13 -0300

    Add SQL implementation of Repository

---
+++ sql.go
@@ -0,0 +1,95 @@
+package repository
+
+import (
+	"context"
+	"gorm.io/gorm"
+)
+
+type SQL[E any] struct {
+	db *gorm.DB
+}
+
+func (r *SQL[E]) Create(ctx context.Context, entity E) (E, error) {
+	if err := r.db.WithContext(ctx).Model(new(E)).Create(&entity).Error; err != nil {
+		var zero E
+		return zero, err
+	}
+	return entity, nil
+}
+
+func (r *SQL[E]) CreateBulk(ctx context.Context, entities []E) ([]E, error) {
+	if err := r.db.WithContext(ctx).Model(new(E)).Create(&entities).Error; err != nil {
+		return nil, err
+	}
+	return entities, nil
+}
+
+func (r *SQL[E]) Get(ctx context.Context, id uint) (E, error) {
+	var out E
+	if err := r.db.WithContext(ctx).Model(new(E)).Where("id = ?", id).First(&out).Error; err != nil {
+		var zero E
+		return zero, err
+	}
+	return out, nil
+}
+
+func (r *SQL[E]) Find(ctx context.Context, ids []uint) ([]E, error) {
+	var out []E
+	if err := r.db.WithContext(ctx).Model(new(E)).Where("id IN (?)", ids).Find(&out).Error; err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (r *SQL[E]) Update(ctx context.Context, id uint, entity E) (E, error) {
+	if err := r.db.WithContext(ctx).Model(new(E)).Where("id = ?", id).Updates(&entity).Error; err != nil {
+		var zero E
+		return zero, err
+	}
+	return entity, nil
+}
+
+func (r *SQL[E]) UpdateBulk(ctx context.Context, ids []uint, entity E) ([]E, error) {
+	if err := r.db.WithContext(ctx).Model(new(E)).Where("id IN (?)", ids).Updates(&entity).Error; err != nil {
+		return nil, err
+	}
+
+	var result []E
+	if err := r.db.WithContext(ctx).Model(new(E)).Where("id IN (?)", ids).Find(&result).Error; err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func (r *SQL[E]) Remove(ctx context.Context, id uint) (E, error) {
+	entity, err := r.Get(ctx, id)
+	if err != nil {
+		var zero E
+		return zero, err
+	}
+
+	if err := r.db.WithContext(ctx).Model(new(E)).Where("id = ?", id).Delete(&entity).Error; err != nil {
+		var zero E
+		return zero, err
+	}
+
+	return entity, nil
+}
+
+func (r *SQL[E]) RemoveBulk(ctx context.Context, ids []uint) ([]E, error) {
+	result, err := r.Find(ctx, ids)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := r.db.WithContext(ctx).Model(new(E)).Where("id = ?", ids).Delete(&result).Error; err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func NewRepositorySQL[E any]() Repository[E] {
+	return &SQL[E]{}
+}
